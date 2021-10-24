6e0f555072d09798c752c8592fd7419ac10b7284
JimmyS83 <github.jpeter@spamgourmet.com>
2021-10-24 15:18:15 +0200

    tvh-dtv-scan-tables bump

--- packages/addons/addon-depends/tvh-dtv-scan-tables/package.mk
+++ packages/addons/addon-depends/tvh-dtv-scan-tables/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tvh-dtv-scan-tables"
-PKG_VERSION="e3138a506a064f6dfd0639d69f383e8e576609da"
-PKG_SHA256="b055e1f91dec43ba268b0feafac1dd23c1ca37613cc429c4be74c0c2606ad253"
+PKG_VERSION="94be51b2e0183faf3ecc3bbf4d6d4b16279b8e7b"
+PKG_SHA256="1879fb5011d5bbda2e2f9f246f2e500c40f85f2d0da4a9bc04ac5a5e0c80f095"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tvheadend"
 PKG_URL="https://github.com/tvheadend/dtv-scan-tables/archive/${PKG_VERSION}.tar.gz"
